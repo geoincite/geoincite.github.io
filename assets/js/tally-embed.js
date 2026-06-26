b4fae7335f98d5813a4ef4853c3808c2e7f9eb37
Farid Javadnejad <nejad.fj@gmail.com>
2026-06-25 20:30:59 -0600

    new web design

---
+++ assets/js/tally-embed.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var s = document.createElement("script");
+  s.src = "https://tally.so/widgets/embed.js";
+  s.async = true;
+  document.body.appendChild(s);
+});
